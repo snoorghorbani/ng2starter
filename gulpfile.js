a21822605bfaf35e1fc9514675604bf7acd25b7e
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-22 01:51:53 +0330

    1.0.7

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,59 @@
 var gulp = require("gulp");
-var gulpCopy = require('gulp-copy');
-var srcProductionPaths = ['./src/build.js', './src/index.html', './src/favicon.png']
-var distProductionPath = '../CustomerCare/production';
-//var typedoc = require("gulp-typedoc");
+var gulpCopy = require("gulp-copy");
+var path = require("path");
+var streamfilter = require("streamfilter");
 
-// gulp.task("typedoc", function () {
-//     var srcTSFiles = ["./src/**/*.ts"];
-//     var srcProductionFiles = ["./src/assets"];
+var srcProductionPaths = [ "./src/build.js", "./src/index.html", "./src/favicon.png" ];
+var distProductionPath = "../CustomerCare/production";
 
-//     return gulp
-//         .src(srcTSFiles)
-//         .pipe(typedoc({
-//             // TypeScript options (see typescript docs) 
-//             module: "commonjs",
-//             target: "ES5",
-//             includeDeclarations: true,
-//             emitDecoratorMetadata: true,
-//             experimentalDecorators: true,
+const copy_file_after_installed = [
+	"./.vscode/**/*",
+	"./configs/**/*",
+	"./db/**/*",
+	"./platforms/**/*",
+	"./scripts/**/*",
+	"./server/**/*",
+	"./src/**/*",
+	"./tslint.json",
+	"./tslint.server.json",
+	"./tslint.json",
+	"./tsconfig-aot.json",
+	"./tsconfig.server.json",
+	"./tsconfig.json",
+	"./README.md",
+	"./karma.conf.js",
+	"./gulpfile.js",
+	"./.gitignore",
+	"./.env.example",
+	"./.angular-cli.json"
+];
 
-//             // Output options (see typedoc docs) 
-//             out: "documentation",
-//             json: "documentation.json",
+var fs = require("fs");
+const filter = function(options) {
+	//console.log('***********************************AMS')
+	options = options || {};
+	var history = {};
+	const srcExists = fs.existsSync("../../../src");
+	return streamfilter(
+		function(file, enc, cb) {
+			if (file.base.includes("src") && srcExists) {
+				cb(true);
+			} else {
+				cb(false);
+			}
+		},
+		{
+			objectMode  : true,
+			passthrough : options.passthough !== false,
+			restore     : options.restore
+		}
+	);
+};
 
-//             // TypeDoc options (see typedoc docs) 
-//             name: "my angular",
-//             version: true,
-//             noLib: true
-//         }));
-// });
+gulp.task("production", [], function() {
+	gulp.src(srcProductionPaths).pipe(gulpCopy(distProductionPath)).pipe(gulp.dest(distProductionPath));
+});
 
-gulp.task("production", [], function () {
-    var streem = gulp.src(srcProductionPaths)
-        .pipe(gulpCopy(distProductionPath))
-        .pipe(gulp.dest(distProductionPath));
-    return streem;
+gulp.task("copy_file_after_installed", [], function() {
+	gulp.src(copy_file_after_installed).pipe(filter()).pipe(gulpCopy("../../..")).pipe(gulp.dest("."));
 });
