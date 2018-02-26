028a27f1dfedb2b2a67216327fc10adbe16c2387
nonokhatiri <nonokhatiri@yahoo.com>
2018-02-26 17:28:45 +0330

    nono
    .

--- server/controllers/form.controller.ts
+++ server/controllers/form.controller.ts
@@ -12,23 +12,20 @@ const router = express.Router();
 router.get("/", function(req, res) {
 	Model.find().then((Result) => res.json({ Result }));
 });
+router.get("/:id", function(req, res) {
+	Model.findById(req.params.id).then((Result) => res.json({ Result }));
+});
 router.post("/", function(req, res) {
 	const model = new Model(req.body);
 	model.save().then((Result) => res.json({ Result })).catch((err) => {
 		debugger;
 	});
 });
-
-// router.get("/:name", function(req, res) {
-// 	Model.Config.findOne({ Name: req.params.name }).then((Result) => res.json({ Result }));
-// });
-
-// router.put("/:name", function(req, res) {
-// 	Model.Config
-// 		.findOneAndUpdate({ Name: req.params.name }, req.body, { upsert: true, new: true })
-// 		.then((config) => res.send(config))
-// 		.catch((err) => {
-// 			debugger;
-// 		});
-// });
+router.put("/:id", function(req, res) {
+	Model.findByIdAndUpdate(req.params.id, req.body, { upsert: true, new: true })
+		.then((config) => res.send(config))
+		.catch((err) => {
+			debugger;
+		});
+});
 export { router };
