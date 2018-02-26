fc3f8130917efcd77b276fc47f04f3db8d8afdf8
nonokhatiri <nonokhatiri@yahoo.com>
2018-02-26 17:00:43 +0330

    .
    نونو

--- src/app/form/form-controls/number/number.component.ts
+++ src/app/form/form-controls/number/number.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { FormFieldSchema } from "app/form/models/form-field-schema.model";
+import { FormGroup } from "@angular/forms";
 
 @Component({
 	selector: "app-number",
 	templateUrl: "./number.component.html"
 })
 export class NumberComponent implements OnInit {
+	@Input() form: FormGroup;
 	@Input() schema: FormFieldSchema;
 	constructor() {}
 
--- src/app/form/form-controls/select/select.component.ts
+++ src/app/form/form-controls/select/select.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { FormFieldSchema } from "app/form/models/form-field-schema.model";
+import { FormGroup } from "@angular/forms";
 
 @Component({
 	selector: "app-select",
 	templateUrl: "./select.component.html"
 })
 export class SelectComponent implements OnInit {
+	@Input() form: FormGroup;
 	@Input() schema: FormFieldSchema;
 	constructor() {}
 	ngOnInit() {}
