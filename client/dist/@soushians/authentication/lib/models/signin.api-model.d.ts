5c90afaafa86d1333e7cd5f35e8a479744942ebd
soushians noorghorbani <snoorghorbani@gmail.com>
2018-07-10 18:41:55 +0430

    6.0.4.4

--- client/dist/@soushians/authentication/lib/models/signin.api-model.d.ts
+++ client/dist/@soushians/authentication/lib/models/signin.api-model.d.ts
@@ -1,8 +1,9 @@
 import { HttpRequestBaseModel, HttpResponseBaseModel } from "@soushians/shared";
 import { UserModel } from "./user.model";
 export declare namespace Signin_ApiModel {
-	class Request implements HttpRequestBaseModel<Request> {}
-	class Response implements HttpResponseBaseModel<Response> {
-		Result: UserModel;
-	}
+    class Request implements HttpRequestBaseModel<Request> {
+    }
+    class Response implements HttpResponseBaseModel<Response> {
+        Result: UserModel;
+    }
 }
