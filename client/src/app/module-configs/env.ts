85995f705f51a37e2c5b1fb9536bd0559aa6216f
pegah <pegah.khf@gmail.com>
2018-07-11 14:57:53 +0430

    .

--- client/src/app/module-configs/env.ts
+++ client/src/app/module-configs/env.ts
@@ -1,5 +1,6 @@
 import { environment } from "../../environments/environment";
 
+// tslint:disable-next-line:class-name
 export class env {
 	static production = environment.production;
 	static frontend_server = environment.frontend_server;
